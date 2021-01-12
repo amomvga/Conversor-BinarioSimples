65a3837501b377dd3d2472d696e814bc42aa6719
amomvga <amom_augusto@hotmail.com>
2021-01-12 15:38:02 -0300

    Projeto Criado

---
+++ src/main/java/Program.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um numero decimal para ser convertido em binario: ");
+        int x = sc.nextInt();
+        String binstr = Integer.toBinaryString(x);
+
+        System.out.println("O numero " + x + " em binario é: " + binstr);
+    }
+}
